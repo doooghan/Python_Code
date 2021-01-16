763bbbc6c185e2c0d96368b77523f1c50c02d088
donghan_5 <donghan_5@tujia.com>
2021-01-16 22:57:30 +0800

    509

---
+++ 509.斐波那契数.py
@@ -0,0 +1,17 @@
+
+# 执行不通过,递归太深
+"""
+class Solution:
+    def fib(self, n: int) -> int:
+        if n == 1 or n == 2:
+            return 1
+        return self.fib(n - 1) + self.fib(n - 2)
+"""
+
+class Solution:
+    def fib(self, n: int) -> int:
+        pass
+
+
+fib = Solution()
+print(fib.fib(20))
